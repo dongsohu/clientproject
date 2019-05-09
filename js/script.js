22c29bf107fe110abd74388ab9a8333bde4ed39d
dongsohu <ongsohu.d@gmail.com>
2019-05-09 22:19:47 +0000

    DAY 2 Pt1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+
+$("#sbutton").click(function(){
+            $("#galore").hide();
+            var bput =$("#binput").val();
+$.ajax({
+        url: `https://www.googleapis.com/books/v1/volumes?q=${bput}`,
+        method: "GET",
+        success: function(response){
+            console.log(response);
+            $("body2").append(response.items[0].selfLink);
+
+
+        },
+    });
+     });
